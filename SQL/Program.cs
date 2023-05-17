7b62290f98a3d01bcb2d3330e276005156e11d4c
unknown <@ngknn.local>
2023-05-17 17:36:41 +0300

    Сравнение видеокарт

--- SQL/Program.cs
+++ SQL/Program.cs
@@ -20,36 +20,77 @@ namespace SQL
             }
             Console.WriteLine(sqlConnection.Database);
             string sqlQuery1 = "SELECT * FROM CPU";
+            string sqlQuery2 = "SELECT * FROM VideoCard";
 
-             sqlConnection.Open();
+            sqlConnection.Open();
 
             SqlCommand command = new SqlCommand(sqlQuery1, sqlConnection);
-            SqlDataReader reader =  command.ExecuteReader();
+            SqlCommand command1 = new SqlCommand(sqlQuery2, sqlConnection);
 
-            if(reader.HasRows)
+            using (SqlDataReader reader = command.ExecuteReader())
             {
+
+
+
+
                 string columnName1 = reader.GetName(0);
                 string columnName4 = reader.GetName(2);
-                
 
-                
+
+
 
                 Console.WriteLine($"{columnName1}\t\t{columnName4}");
 
-                for(int i = 0; reader.Read();i++)
+                for (int i = 0; reader.Read(); i++)
                 {
-                    
-                    for(int j = 0; j<1;j++)
+
+                    for (int j = 0; j < 1; j++)
                     {
-                        CPU[i, j] = Convert.ToString (reader.GetInt32(0));
-                        CPU[i, j+1] = reader.GetString(2);
+                        CPU[i, j] = Convert.ToString(reader.GetInt32(0));
+                        CPU[i, j + 1] = reader.GetString(2);
                         Console.WriteLine(CPU[i, j] + "\t\t" + CPU[i, j + 1]);
                     }
-                    
+
                 }
                 Console.WriteLine("Я валера");
             }
+            using (SqlDataReader reader = command1.ExecuteReader())
+            {
+                string[,] arr1 = new string[2, 2];
+                Console.WriteLine("Введите первую видеокарту");
+                arr1[0, 1] = Console.ReadLine();
+                Console.WriteLine("Введите первую видеокарту");
+                arr1[1, 1] = Console.ReadLine();
+
+
+
+                for (int i = 0; reader.Read(); i++)
+                {
+                    if (reader.GetString(2) == arr1[0, 1])
+                    {
+                        arr1[0, 0] = Convert.ToString(reader.GetInt32(0));
+                    }
+                    if (reader.GetString(2) == arr1[1, 1])
+                    {
+                        arr1[1, 0] = Convert.ToString(reader.GetInt32(0));
+                    }
+                }
+                Console.WriteLine(arr1[0, 0] + " " + arr1[1, 0]);
+                if(Convert.ToInt32(arr1[0, 0]) < Convert.ToInt32(arr1[1, 0]))
+                {
+                    Console.WriteLine(arr1[0, 1]);
+                }
+                else
+                {
+                    Console.WriteLine(arr1[1,1]);
+                }
+            }
+               
+
+
+            
 
         }
+        
     }
 }
